c76a4990eb7ec429cd374331007d6c705e05813f
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2015-09-11 14:19:29 +0300

    Update initLicode.sh

--- scripts/initLicode.sh
+++ scripts/initLicode.sh
@@ -11,11 +11,6 @@ SCRIPTS=$ROOT/scripts
 
 export PATH=$PATH:/usr/local/sbin
 
-if ! pgrep -f rabbitmq; then
-  sudo echo
-  sudo rabbitmq-server > $BUILD_DIR/rabbit.log &
-fi
-
 cd $ROOT/nuve
 ./initNuve.sh
 
